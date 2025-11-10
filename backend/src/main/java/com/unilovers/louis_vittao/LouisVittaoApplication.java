afd714dcf7eff9a0bb9ae94b53b49516b828c50a
Victor Andrade <victor64gaming@gmail.com>
2025-11-10 20:18:59 -0300

    Configuração inicial do backend

---
+++ backend/src/main/java/com/unilovers/louis_vittao/LouisVittaoApplication.java
@@ -0,0 +1,13 @@
+package com.unilovers.louis_vittao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LouisVittaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LouisVittaoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/unilovers/louis_vittao/LouisVittaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unilovers.louis_vittao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LouisVittaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
